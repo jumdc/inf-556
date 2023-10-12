3d14656588ec59fd286dc44549fdbf1147a59ac1
jumdc <julie.mordacq@live.fr>
2023-10-12 10:34:02 +0200

    add strat TD4

---
+++ TD4/main.py
@@ -0,0 +1,21 @@
+import logging
+import pyrootutils
+import os
+
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+
+from TD4.utils import ComputePersistence
+
+
+if __name__ == "__main__":
+    persistence = ComputePersistence("TD4/data/filtration.xy")
+    persistence.compute_boundary()
+    reduced = persistence.gaussian_elimination()
+    barcode = persistence.barcode_output(reduced)
+    for bar in barcode: 
+        print(bar)
